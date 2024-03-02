b892e0e32a88e8fe1490b81581861043adad155a
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 15:48:45 +0700

    Comparisons

---
+++ comparison.py
@@ -0,0 +1,19 @@
+# ==
+print(7 == 8) # False
+print('a' == 'a') # True
+
+# !=
+print(7 != 7) # False
+print('a' != 'A') # True
+
+# >
+print(7 > 9) # False
+
+# =
+print(7 >= 7) # True
+
+# <
+print(7 < 4) # False
+
+# <=
+print(7 <= 9) # True
