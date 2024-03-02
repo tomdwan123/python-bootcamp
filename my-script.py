7c67015b3731cb5331185ea6d6be55294b2562d3
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 14:21:43 +0700

    Variables

--- my-script.py
+++
@@ -1,7 +0,0 @@
-print('Hello Python')
-
-x = 10
-print(x * 2)
-
-y = 20
-print(y * 3)
---
+++ variables.py
@@ -0,0 +1,9 @@
+miles = 5
+miles = 10
+
+a, b, c = 5, 10, 15
+print(a)
+print(b)
+print(c)
+
+age, name = 20, "John"
