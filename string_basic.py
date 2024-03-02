3616e8dd3e9f7c0bbc24a87f4c9c1993c1d07959
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 17:12:06 +0700

    Strings

---
+++ string_basic.py
@@ -0,0 +1,21 @@
+my_str1 = 'I learn a Python'
+my_str2 = "I learn a Python"
+print(my_str1 == my_str2)
+
+print('Hello I\'m okay')
+print("Hello I'm okay")
+
+message = 'He said: "Hello"'
+print(type(message))
+
+languages = '''I like python
+golang
+and
+java
+'''
+print(languages)
+
+my_languages = "I like python\ngolang\nand\njava"
+print((my_languages))
+
+print('He says: "I\'m 20"')
