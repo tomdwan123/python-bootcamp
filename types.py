377d231e5d298ff9e50b77ed4e4f7841cbe30494
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 15:05:26 +0700

    Types

---
+++ types.py
@@ -0,0 +1,11 @@
+# Numbers: int, float, complex
+
+# Booleans: True, False
+
+# Strings:  language = "Java, Python"
+
+# Lists and Tuples: cities = ["NYC", "London"]
+
+# Sets: states = {"CA", "NY"}
+
+# Dictionaries: friend = {"name": "Tom", age: 30}
