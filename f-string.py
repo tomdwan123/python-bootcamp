fade052b232d84d57085693df53b5e97638ce782
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 20:28:19 +0700

    f-string

---
+++ f-string.py
@@ -0,0 +1,8 @@
+first_name = "John"
+last_name = "Smith"
+age = 20
+print("Hello " + first_name + " " + last_name + "! You are " + str(age) + " years old")
+print(f"Hello {first_name} {last_name}! You are {age} years old")
+
+s = f'{2.3 * 3.2 / 4.1: .2f}'
+print(s)
