4ca69dffb015d5002a90ac25dbb5f63983855c02
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 17:22:16 +0700

    Get user input

---
+++ getting_user_input.py
@@ -0,0 +1,7 @@
+name = input('Enter your name: ')
+print('Your name is ', name)
+
+price = input("Enter price: ")
+quantity = input("Enter quantity: ")
+total_value = float(price) * float(quantity)
+print(total_value)
