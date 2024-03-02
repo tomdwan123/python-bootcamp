bae9e68f8ee8911109e9f7bf147c5f2d584baf63
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 15:40:09 +0700

    Operators

--- operartors.py
+++ operartors.py
@@ -23,3 +23,27 @@ print(2 + 4 * 2 ** 3) # 34
 print((2 + 4) * 2 ** 3) # 48
 
 print(1_000_000_000)
+
+# divmod()
+a, b = divmod(14, 6)
+print(a, b)
+
+# pow()
+c = pow(2, 3)
+print(c)
+
+# sum()
+d = sum([1, 2, 3, 4])
+print(d)
+
+# max()
+e = max([1, 2, 3, 4])
+print(e)
+
+# min()
+f = min([1, 2, 3, 4])
+print(f)
+
+# round()
+g = 5.66749
+print(round(g, 3))
