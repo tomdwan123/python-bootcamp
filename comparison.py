b1e4472bf884e13e3bc9040e76b7a788a7bb2b76
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 16:50:34 +0700

    Comparisons

--- comparison.py
+++ comparison.py
@@ -36,3 +36,29 @@ li = l.copy()
 print(id(li))
 print(l == li) # compare value
 print(l is li) # compare address
+
+# format()
+print(format(1/3, '.5f')) # 0.33333
+
+a = 0.1 * 3
+b = 0.3
+print(format(a, '.25f'))
+print(format(b, '.25f'))
+print(a == b) # False
+
+from math import isclose
+x = 0.0000000001
+y = 0.0000000002
+print(x == y) # False
+print(isclose(x, y, abs_tol=0.01)) # True
+
+a = 999999999.01
+b = 999999999.02
+print(a == b) # False
+print(isclose(a, b, rel_tol=0.01)) # True
+
+a = 3.4
+b = 2.3
+print(format(a, '.25f'))
+print(format(b, '.25f'))
+print(a + b) # is not 5.7
