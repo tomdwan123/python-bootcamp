bf4d26daa271689ec4cb42768055da9ee206bf80
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 15:59:39 +0700

    Comparisons

--- comparison.py
+++ comparison.py
@@ -17,3 +17,22 @@ print(7 < 4) # False
 
 # <=
 print(7 <= 9) # True
+
+# is
+a, b = 3, 4
+print(a is b)
+
+# is not
+print(a is not b)
+
+# append()
+l = [1, 2, 3]
+print(id(l))
+l.append(4)
+print(id(l)) # list immutable
+
+# copy()
+li = l.copy()
+print(id(li))
+print(l == li) # compare value
+print(l is li) # compare address
