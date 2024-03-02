89f4b68945db77b92c501bf855fc4bc953fa6e54
tomdwan123 <tomdwanno1@gmail.com>
2024-03-02 19:04:10 +0700

    String concatenation and repetition

---
+++ string_concatenation_repetition.py
@@ -0,0 +1,15 @@
+movie = "The Godfather"
+director = "Tony"
+movie_director = movie + director
+print(movie_director)
+print(movie_director + " is directed by " + director)
+
+print("abc" "123")
+
+language = "Python "
+version = 3.11
+print(language + str(version))
+
+# repetition
+print(movie * 5)
+print('#' * 5)
