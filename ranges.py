793a3cbe24503180c31bdc3353dc63096dcd4a96
tomdwan123 <tomdwanno1@gmail.com>
2024-03-03 09:52:24 +0700

    ranges

---
+++ ranges.py
@@ -0,0 +1,15 @@
+r = range(2, 10)
+print(type(r))
+print(list(r))
+
+r = range(2, 10, 3)
+print(list(r))
+
+# range(stop)
+print(list(range(10)))
+
+# range(start, stop)
+print(list(range(3, 9)))
+
+# range(start, stop, step)
+print(list(range(0, 10, 3)))
